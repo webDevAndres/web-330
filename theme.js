7a8f359f28e412f88c75907ce03b922ba6893d08
Andres Macias <macias.andres02@gmail.com>
2022-05-27 22:05:42 -0500

    starter theme js

---
+++ theme.js
@@ -0,0 +1,28 @@
+/**
+    Title: theme.js
+    Author: Andres Macias
+    Date: 5/27/2021
+    Description: JS theme file for WEB 330 - Enterprise JavaScript II
+*/
+
+/**
+ * Set the default theme to light if one has not been set in the browsers localStorage
+ */
+ function setDefaultTheme()
+ {
+     const theme = localStorage.getItem("mode") || "light-theme";
+     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+     const iconText = localStorage.getItem("iconText") || "Light Mode";
+ 
+     document.body.classList.value = theme;
+     document.getElementById("icon-mode").classList.add(iconMode);
+     document.getElementById("icon-text").innerHTML = iconText;
+ }
+ 
+ /**
+  * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
+  */
+ function setSelectedTheme()
+ {
+     document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+ }

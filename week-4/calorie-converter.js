98483158d5f6e330922a8deff2a4765df5f3d4bc
Andres Macias <macias.andres02@gmail.com>
2022-06-13 12:01:22 -0500

    add static variable that holds 6 objects

--- week-4/calorie-converter.js
+++ week-4/calorie-converter.js
@@ -1,15 +1,24 @@
+/*
+TODO:
+- create a static function with a single parameter
+use javaScripts built in filter function to return
+a new array of data with the objects matching the parameter string.
+Use the FoodModel name field to compare the parameter string value against.
+
+
+
+
+*/
 import { FoodModel } from "./food-model";
 
 export class CalorieConverter{
-   static data = {};
-   data = new FoodModel(1007, "Egg", 78);
-   data = new FoodModel(1008, "Apple", 95);
-   data = new FoodModel(1009, "Hamburger", 354);
-   data = new FoodModel(1010, "Fries", 400);
-   data = new FoodModel(1011, "Banana", 105);
-   data = new FoodModel(1012, "Soda", 150);
-
-   static find(string) {
-    
-   }
-}
+   static data = [
+   new FoodModel(1007, "Egg", 78),
+   new FoodModel(1008, "Apple", 95),
+   new FoodModel(1009, "Hamburger", 354),
+   new FoodModel(1010, "Fries", 400),
+   new FoodModel(1011, "Banana", 105),
+   new FoodModel(1012, "Soda", 150)
+   ];
+}  
+console.log(data);

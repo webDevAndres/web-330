3c8ea05497414e4c1e198d6027d7a9100ac0192f
Andres Macias <macias.andres02@gmail.com>
2022-06-13 11:47:52 -0500

    add classes

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,15 @@
+import { FoodModel } from "./food-model";
+
+export class CalorieConverter{
+   static data = {};
+   data = new FoodModel(1007, "Egg", 78);
+   data = new FoodModel(1008, "Apple", 95);
+   data = new FoodModel(1009, "Hamburger", 354);
+   data = new FoodModel(1010, "Fries", 400);
+   data = new FoodModel(1011, "Banana", 105);
+   data = new FoodModel(1012, "Soda", 150);
+
+   static find(string) {
+    
+   }
+}
---
+++ week-4/food-model.js
@@ -0,0 +1,8 @@
+
+export class FoodModel {
+    constructor(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+}

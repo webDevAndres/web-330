ec1a205acc2be61dd4788833b2a0b7a38badba01
Andres Macias <macias.andres02@gmail.com>
2022-07-03 20:28:21 -0500

    add finanance calc class

---
+++ week-6/finance-calculator.js
@@ -0,0 +1,30 @@
+/*
+Title: finance-calculator.js
+Author: Andres Macias
+Date: 7/03/2022
+Description:
+*/
+
+export class FinanceCalculator {
+    static MONTHS_IN_YEAR = 12;
+    static calculateFutureValue(monthlyPayment, rate, years) {
+        let months = this.MONTHS_IN_YEAR;
+        let interestRate = 1 + rate / 100;
+        let presentValue = monthlyPayment * months;
+
+        let futureValue = presentValue * (Math.pow(interestRate, months));
+
+        return futureValue.toFixed(2);
+    }
+
+    static convertToCurrency(field) {
+        let currencyFormatter = new Intl.NumberFormat('en-Us',{
+            style: "currency",
+            currency: "USD"
+        });
+
+        return currencyFormatter.format(field);
+    }
+
+
+}
